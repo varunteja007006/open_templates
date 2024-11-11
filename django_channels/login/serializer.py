e66137a5da91bbd58b6cf28ff8e55cf0a74b0e48
varun <varunteja007006@gmail.com>
2024-11-11 23:38:29 +0530

    remove channels

--- django_channels/login/serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-
-
-class CustomTokenObtainPairViewSerializer(TokenObtainPairSerializer):
-    @classmethod
-    def get_token(cls, user):
-        token = super().get_token(user)
-        # add custom items in the token
-        return token
