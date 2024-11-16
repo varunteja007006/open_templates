38d0bc332d21cf8c299240c5d8d121369c1f726f
varun <varunteja007006@gmail.com>
2024-11-16 23:22:29 +0530

    update with express app

---
+++ node/index.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
