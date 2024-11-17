a9b3a5441fdf2efd6fb378e243c803dc35b0ce85
varun <varunteja007006@gmail.com>
2024-11-17 13:13:52 +0530

    update the db connection

--- node/drizzle.config.ts
+++ node/drizzle.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
   dbCredentials: {
-    host: process.env.DATABASE_HOST || "localhost",
+    host: process.env.DATABASE_HOST ?? "localhost",
     port: 5432,
     user: process.env.DATABASE_USER ?? "",
     password: process.env.DATABASE_PASSWORD ?? "",
--- node/src/db.ts
+++ node/src/db.ts
@@ -5,12 +5,12 @@ import { drizzle } from "drizzle-orm/node-postgres";
 // You can specify any property from the node-postgres connection options
 const db = drizzle({
   connection: {
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    //   connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
+    host: process.env.DATABASE_HOST ?? "localhost",
+    port: 5432,
+    user: process.env.DATABASE_USER ?? "",
+    password: process.env.DATABASE_PASSWORD ?? "",
+    database: process.env.DATABASE_NAME ?? "",
     ssl: false,
   },
 });
