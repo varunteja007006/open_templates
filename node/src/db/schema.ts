41c8611a65a080ccbbcce03714d26449fd5902ef
varun <varunteja007006@gmail.com>
2024-11-17 14:28:35 +0530

    update with drizzle

--- node/src/db/schema.ts
+++ node/src/db/schema.ts
@@ -1,8 +1,44 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import {
+  boolean,
+  integer,
+  pgTable,
+  timestamp,
+  unique,
+  varchar,
+} from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("test_drizzle", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
-});
+export const authUser = pgTable(
+  "auth_user",
+  {
+    id: integer("id").primaryKey().notNull().generatedByDefaultAsIdentity({
+      name: "null",
+      startWith: undefined,
+      increment: undefined,
+      minValue: undefined,
+      maxValue: undefined,
+      cache: undefined,
+    }),
+    password: varchar("password", { length: 128 }).notNull(),
+    lastLogin: timestamp("last_login", { withTimezone: true, mode: "string" }),
+    isSuperuser: boolean("is_superuser").notNull(),
+    username: varchar("username", { length: 150 }).notNull(),
+    firstName: varchar("first_name", { length: 150 }).notNull(),
+    lastName: varchar("last_name", { length: 150 }).notNull(),
+    email: varchar("email", { length: 254 }).notNull(),
+    isStaff: boolean("is_staff").notNull(),
+    isActive: boolean("is_active").notNull(),
+    dateJoined: timestamp("date_joined", {
+      withTimezone: true,
+      mode: "string",
+    }).notNull(),
+  },
+  (table) => {
+    return [
+      {
+        authUserUsernameKey: unique("auth_user_username_key").on(
+          table.username
+        ),
+      },
+    ];
+  }
+);
--- node/src/index.ts
+++ node/src/index.ts
@@ -1,39 +1,9 @@
-// Make sure to install the 'pg' package
-import { eq } from "drizzle-orm";
-import { usersTable } from "./db/schema";
 import db from "./db";
+import { authUser } from "./db/schema";
 
 async function main() {
-  const user: typeof usersTable.$inferInsert = {
-    name: "John",
-    age: 30,
-    email: "john@example.com",
-  };
-
-  await db.insert(usersTable).values(user);
-  console.log("New user created!");
-
-  const users = await db.select().from(usersTable);
-  console.log("Getting all users from the database: ", users);
-  /*
-  const users: {
-    id: number;
-    name: string;
-    age: number;
-    email: string;
-  }[]
-  */
-
-  await db
-    .update(usersTable)
-    .set({
-      age: 31,
-    })
-    .where(eq(usersTable.email, user.email));
-  console.log("User info updated!");
-
-  await db.delete(usersTable).where(eq(usersTable.email, user.email));
-  console.log("User deleted!");
+  const authUsers = await db.select().from(authUser);
+  console.log(authUsers);
 }
 
 main();
