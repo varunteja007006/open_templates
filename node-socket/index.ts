2f611b440355e3d3875285a1ac71bed0f5ef918b
varun <varunteja007006@gmail.com>
2024-11-24 17:41:47 +0530

    update the build script npm run build

--- node-socket/index.ts
+++ node-socket/index.ts
@@ -10,23 +10,31 @@ import logger from "@/middleware/logger/logger";
 import onConnection from "./connections";
 import socketLogger from "@/middleware/socket-logger/socket-logger";
 
+// env variables
+const EXPRESS_PORT = process.env.EXPRESS_PORT ?? 4000;
+const CORS_ORIGIN = process.env.CORS_ORIGIN ?? "http://localhost:3000";
+
 const app = express();
 
 app.use(express.json());
-
 app.use(cors());
 
+// middleware
 app.use(logger);
 
-//Add routes
-app.use("/api/", (req, res) => {
-  res.status(200).json({ message: "Hello World" });
+// Add routes
+// health check
+app.use("/api/health-check", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+    message: `Server is running on port ${EXPRESS_PORT}, CORS_ORIGIN: ${CORS_ORIGIN}`,
+  });
 });
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: process.env.CORS_ORIGIN ?? "http://localhost:3000",
+    origin: CORS_ORIGIN ?? "http://localhost:3000",
     credentials: true,
   },
 });
@@ -37,13 +45,13 @@ io.use(socketLogger);
 io.on("connection", onConnection);
 
 httpServer
-  .listen(process.env.EXPRESS_PORT ?? 4000, () => {
-    console.log("Running on port : ", process.env.EXPRESS_PORT ?? 4000);
+  .listen(EXPRESS_PORT ?? 4000, () => {
+    console.log("Running on port : ", EXPRESS_PORT ?? 4000);
   })
   .on("error", (err: Error) => {
     console.log(err);
   });
 
-// Using app.listen(3000) will not work here, as it creates a new HTTP server.
+// ! Using app.listen(3000) will not work here, as it creates a new HTTP server.
 
 export { io };
