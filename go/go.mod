1529f1d7ef01aab7a78417a5c78fdab20a0bea35
varunteja <varunteja007006@gmail.com>
2025-01-25 20:36:04 +0530

    add chi router and middleware to log api requests

--- go/go.mod
+++ go/go.mod
@@ -1,3 +1,5 @@
 module github.com/open_templates/go
 
 go 1.23.5
+
+require github.com/go-chi/chi/v5 v5.2.0
--- go/main.go
+++ go/main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	router := chi.NewRouter()
+
+	router.Use(middleware.Logger)
+
+	router.Get("/hello", basicHandler)
+
 	server := &http.Server{
 		Addr:    ":4040",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 
 	err := server.ListenAndServe()
