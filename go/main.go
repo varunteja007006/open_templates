4d3525a02a2d425d621f8e04160597b1f66557c3
varunteja <varunteja007006@gmail.com>
2025-01-25 20:05:23 +0530

    go initialize the server

--- go/main.go
+++ go/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello world")
+	server := &http.Server{
+		Addr:    ":4040",
+		Handler: http.HandlerFunc(basicHandler),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
 }
