224b64660968ecd499ff60ff801f618181977b21
varun teja <varunteja007006@gmail.com>
2025-11-04 23:12:54 +0530

    ✅ : backend trpc standalone

--- my-turbo-app/apps/next-trpc/app/page.tsx
+++ my-turbo-app/apps/next-trpc/app/page.tsx
@@ -1,19 +1,16 @@
 import { ModeToggle } from "@/components/mode-toggle";
-import { add } from "@workspace/starter-package";
-
 import { Button } from "@workspace/ui/components/button";
+import TRPCDemo from "./trpc-demo";
 
 export default function Page() {
-  const sum = add(2, 3);
-
-  return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <p>{sum}</p>
-        <Button size="sm">Button</Button>
-        <ModeToggle />
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex items-center justify-center min-h-svh">
+			<div className="flex flex-col items-center justify-center gap-4">
+				<h1 className="text-2xl font-bold">Hello World</h1>
+				<Button size="sm">Button</Button>
+				<ModeToggle />
+				<TRPCDemo />
+			</div>
+		</div>
+	);
 }
--- my-turbo-app/apps/next-trpc/components/providers.tsx
+++ my-turbo-app/apps/next-trpc/components/providers.tsx
@@ -2,17 +2,22 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ReactQueryTRPCProvider } from "@/lib/trpc/client/provider";
+import { Toaster } from "@workspace/ui/components/sonner";
 
-export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <NextThemesProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-      enableColorScheme
-    >
-      {children}
-    </NextThemesProvider>
-  );
+export function Providers({
+	children,
+}: Readonly<{ children: React.ReactNode }>) {
+	return (
+		<NextThemesProvider
+			attribute="class"
+			defaultTheme="system"
+			enableSystem
+			disableTransitionOnChange
+			enableColorScheme
+		>
+			<ReactQueryTRPCProvider>{children}</ReactQueryTRPCProvider>
+			<Toaster />
+		</NextThemesProvider>
+	);
 }
