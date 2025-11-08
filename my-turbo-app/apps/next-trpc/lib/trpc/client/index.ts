8dde82a0831e194aa7768089b6ed8520280d394a
varun teja <varunteja007006@gmail.com>
2025-11-08 14:54:01 +0530

    ✅ : update the projects

--- my-turbo-app/apps/next-trpc/lib/trpc/client/index.ts
+++
@@ -1,5 +0,0 @@
-import { createTRPCContext } from "@trpc/tanstack-react-query";
-import type { AppRouter } from "@/backend/server";
-
-export const { TRPCProvider, useTRPC, useTRPCClient } =
-	createTRPCContext<AppRouter>();
